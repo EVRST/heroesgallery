bf15374fd68d0abf2ecbebdfd19b06220e75095f
Laetitia Massa <laetitiamassa@gmail.com>
2016-08-08 20:45:47 +0200

    added news feature

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ActiveRecord::Base
+	has_attached_file :image, styles: { large: "900x510#", medium: "600x340#", thumb: "270x270#" }, default_url: ":style/missing.png"
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end

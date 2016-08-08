805b09c3093823766cea21c86ebcaf947b77436b
Laetitia Massa <laetitiamassa@gmail.com>
2016-08-08 22:33:00 +0200

    added show page for books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ActiveRecord::Base
-	has_attached_file :image, styles: { large: "976x600#", medium: "600x300#", little: "600x270#", thumb: "270x270#" }, default_url: ":style/missing.png"
+	has_attached_file :image, styles: { large: "976x600#", medium: "300x600#", little: "600x270#", thumb: "270x270#" }, default_url: ":style/missing.png"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
     has_attached_file :miniature, styles: { large: "976x600#", medium: "600x300#", little: "600x270#", thumb: "270x270#" }, default_url: ":style/missing.png"

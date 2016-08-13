c31af3d316b371773672c6db5d983aa2f45ff3e2
Laetitia Massa <laetitiamassa@gmail.com>
2016-08-13 13:45:33 +0200

    added styling in checkout page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,24 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-    
+    def full_name
+    	"#{first_name} #{last_name}"
+    end
+
+    def shipping_address
+    	"#{shipping_street}, #{shipping_number}"
+    end
+
+    def shipping_town
+    	"#{shipping_zipcode} #{shipping_city}"
+    end
+
+    def billing_address
+    	"#{billing_street}, #{billing_number}"
+    end
+
+    def billing_town
+    	"#{billing_zipcode} #{billing_city}"
+    end
 
 end

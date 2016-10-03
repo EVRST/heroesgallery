ed724db96e5f8521b5e458c78ce536e35e696c9c
Laetitia Massa <laetitiamassa@gmail.com>
2016-10-03 22:17:44 +0200

    added number and line in finitions page

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,5 @@ class Book < ActiveRecord::Base
 
     has_attached_file :miniature, styles: { large: "976x600#", medium: "600x300#", little: "600x270#", thumb: "270x270#" }, default_url: ":style/missing.png"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
 end
